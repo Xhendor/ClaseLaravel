a3f6f37cb89546ea2a8a411841d27d87c332a18c
Xhendor <rosendorsc@gmail.com>
2018-10-08 17:10:10 -0700

    Convert to use Heroku PostgreSQL database 5

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 $url = parse_url(getenv('postgres://ubpcugyduqswnp:193a4a04f16bf132a927f57c2df9778ef0b6a753c0cca3bce23513bacf1ed7f3@ec2-23-23-80-20.compute-1.amazonaws.com:5432/df1gvhm65t6lc5'));
 
-$host = $url['host'];
-$username = $url['user'];
-$password = $url['pass'];
-$database = substr($url['path'], 1);
+$host = "c2-23-23-80-20.compute-1.amazonaws.com";
+$username = "ubpcugyduqswnp";
+$password = "193a4a04f16bf132a927f57c2df9778ef0b6a753c0cca3bce23513bacf1ed7f";
+$database = "df1gvhm65t6lc5";
 return [
 
     /*
@@ -82,7 +82,6 @@ return [
         ],
 
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
