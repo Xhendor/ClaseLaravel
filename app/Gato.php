66c32f6856ddf4ac0b1c1a2396b0f6b800c62037
Xhendor <rosendorsc@gmail.com>
2018-10-17 08:14:52 -0700

    Se agrego a gato hidden atributos

--- app/Gato.php
+++ app/Gato.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Gato extends Model
 {
 
+    protected $hidden=['raza','color'];
+
     public function animal(){
 
        return $this->belongsTo(Animal::class);
