86a715db70c9e6563f6cda3008f1679a46ffa5c0
Xhendor <rosendorsc@gmail.com>
2018-10-17 17:54:26 -0700

    Add Animal migration and relationship example 4

--- app/Animal.php
+++ app/Animal.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Animal extends Model
 {
     protected $table='animales';
+
+
+    public function gatos(){
+        return $this->hasMany(Gato::class);
+    }
 }
--- app/Gato.php
+++ app/Gato.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gato extends Model
 {
-
     protected $hidden=['raza','color'];
 
     public function animal(){
